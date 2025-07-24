ed864d32e85388ab2cbb2a017442ca76b7d5d15d
Alan Haynes <aghaynes@users.noreply.github.com>
2025-07-24 11:43:18 +0200

    add example code for rendering a qmd file

--- inst/extdata/R/master.R
+++ inst/extdata/R/master.R
@@ -5,6 +5,21 @@ source(paths$rs("02_dataprep.R"))
 source(paths$rs("03_baseline.R"))
 source(paths$rs("04_analysis.R"))
 
+library(quarto)
+quarto_render(paths$rd("QMD-FILENAME.qmd"),
+              # # additional parameters:
+              # execute_params = list(dat = all_data),
+              # metadata = list(
+              #   # e.g. subtitle (remove subtitle from the qmd header)
+              #   subtitle = paste("Recruitment report,", Sys.Date()),
+              #   # e.g. quarto (not R) variable that can be used to hide content
+              #   foo = list(closed = TRUE))
+)
+file.copy(paths$rd("QMD-FILENAME.pdf"),
+          paths$rd(paste0("OUTPUT-FILENAME",
+                          Sys.Date(),
+                          ".pdf")), overwrite = TRUE)
+
 
 # package control through renv ----
 # initialize
